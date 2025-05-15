bca9413141d602a6e4edf3138b1022d3aa68d76a
lolvlolv <lolvlolv123@gmail.com>
2025-05-15 18:35:03 +0200

    dodanie struktury DB

--- baza_danych_oracle/C##SZKOLA/body/OSOBA_TYPE.sql
+++ baza_danych_oracle/C##SZKOLA/body/OSOBA_TYPE.sql
@@ -1,2 +1,31 @@
--- The source text is lost (family: ORACLE, kind: body, name:OSOBA_TYPE, source text length: 851)
+CREATE OR REPLACE TYPE BODY osoba_type AS
+-- Implementacja metod dla typu obiektowego osoba_type
+-- Procedura set_pesel w przypadku podania niewłasciwego
+-- numeru pesel zglosi wyjatek APP_ERROR
+ MEMBER PROCEDURE set_pesel(p_pesel IN VARCHAR2) IS
+ BEGIN
+   IF LENGTH(p_pesel) = 11
+      AND REGEXP_LIKE(p_pesel, '^\d{11}$') THEN
+          --ustawienie pola pesel po waidacji
+     SELF.pesel := p_pesel;
+   ELSE
+       -- blad jezeli format jest niewlasciwyS
+     RAISE_APPLICATION_ERROR(-20001,
+       'Nieprawidłowy PESEL: musi zawierać dokładnie 11 cyfr.');
+   END IF;
+ END;
 
+
+--get_pesel- zwrocenie numeru przechowywanego w obiekcie
+ MEMBER FUNCTION get_pesel RETURN VARCHAR2 IS
+ BEGIN
+   RETURN SELF.pesel;
+ END;
+-- polaczenie imienia i nazwiska z separatorem
+ MEMBER FUNCTION pelne_nazwisko RETURN VARCHAR2 IS
+ BEGIN
+   RETURN imie || ' ' || nazwisko;
+ END;
+
+
+END;
